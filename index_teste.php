ffddb83b739fde5423235a1b5b7ac398e774a4d5
brunomacedodev <macedodeoliveirabruno@gmail.com>
2019-10-01 16:38:16 -0300

    index_text.php com cards promocionais feitos com foreach

--- index_teste.php
+++ index_teste.php
@@ -16,14 +16,16 @@
     <link rel="stylesheet" href="assets\css\header_style.css">
     <link rel="icon" href="assets/img/favicon_taverna_48x48.png" sizes="96x96" type="image/png">
 
-    <style>
+    <!-- <style>
         * {
-            /* border: .5px solid forestgreen; */
+            border: .5px solid forestgreen;
         }
-    </style>
+    </style> -->
 </head>
 
 <body>
+    <?php include_once('utils\produtosCards.php') ?>
+
     <main class="container-fluid">
 
         <header>
@@ -93,37 +95,83 @@
         </header>
 
         <!-- CAROUSEL DE DESTAQUES -->
-        <section class="row d-flex justify-content-center">
 
-            <div class="col-12 p-0 m-0 text-center bg-warning" style="height: 225px;">
-                <h1 class="text-white">carousel principal</h1>
-            </div>
-
-            <!-- CARDS PROMOCIONAIS -->
-            <div class="row p-0 m-0 bg-danger" style="height: 400px;">
-                <div class="col mx-3 my-3">
-                    <h3 class="text-white">card promocional</h3>
+        <div id="carouselHome" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselHome" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselHome" data-slide-to="1"></li>
+                <li data-target="#carouselHome" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="assets\img\banner_cod.jpg" alt="Primeiro Slide">
                 </div>
-                <div class="col mx-3 my-3">
-                    <h3 class="text-white">card promocional</h3>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="assets\img\banner_presentes.jpg" alt="Segundo Slide">
                 </div>
-                <div class="col mx-3 my-3">
-                    <h3 class="text-white">card promocional</h3>
-                </div>
-                <div class="col mx-3 my-3">
-                    <h3 class="text-white">card promocional</h3>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="assets\img\banner_moda.jpg" alt="Terceiro Slide">
                 </div>
             </div>
-
-            <!-- FORMULÁRIO NEWSLETTER -->
-            <div class="col-12 bg-primary" style="height: 300px;">
-                <h1 class="text-white text-center">formulario newsletter</h1>
+            <div class="setasCarousel">
+                <a class="carousel-control-prev" href="#carouselHome" role="button" data-slide="prev">
+                    <i class="fas fa-caret-left"></i>
+                    <span class="sr-only">Anterior</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselHome" role="button" data-slide="next">
+                        <i class="fas fa-caret-right"></i>
+                    <span class="sr-only">Próximo</span>
+                </a>
             </div>
+        </div>
 
-            <!-- FORMULÁRIO NEWSLETTER -->
-            <div class="col-12 bg-secondary" style="height: 300px;">
-                <h1 class="text-white text-center">banner</h1>
-            </div>
+        <!-- CARDS PROMOCIONAIS -->
+
+        <section class="container-fluid my-4 p-0">
+
+            <div class="row m-0 px-0 py-2" style="width: 100%">
+                    
+                <?php foreach ($produtosCard as $nomeProduto => $infosProduto) { ?>
+                    <div class="col border border-white rounded mx-3 p-3">
+                        <img src="<?php echo $infosProduto[0]; ?>" class="card-img-top" alt="">
+                        <div class="row text-center mt-2 px-2">
+                            <h5 class="text-white"><?php echo $infosProduto[1]; ?></h5>
+                        </div>
+                        <div class="row p-0 mt-2 justify-content-center d-flex flex-nowrap">
+                            <ul class="d-flex flex-row text-white col">
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star-half-alt"></i></li>
+                                <li><i class="far fa-star"></i></li>
+                                <li><img src="assets\img\taverna_logo.svg" class="ml-2" style="height: 20px" alt="">
+                                <small class="text-white">taverna indica</small></li>
+                            </ul>
+                        </div>
+                        <div class="row p-0 mt-2 justify-content-center">
+                            <p class="text-white"><strong><?php echo $infosProduto[3]; ?> ofertas a partir de</strong></p>
+                        </div>
+                        <div class="row justify-content-center">
+                            <h2 class="text-white text-center">R$ <?php echo $infosProduto[2]; ?></h2>
+                        </div>
+                        <div class="row justify-content-center mt-2">
+                            <button class="btn btnPadrao" title="Confira Já!">Confira Já! <i class="fas fa-thumbs-up"></i></button>
+                        </div>
+                    </div>
+                <?php } ?>
+                
+        </section>
+
+
+        <!-- FORMULÁRIO NEWSLETTER -->
+        <div class="col-12 bg-primary" style="height: 300px;">
+            <h1 class="text-white text-center">formulario newsletter</h1>
+        </div>
+
+        <!-- FORMULÁRIO NEWSLETTER -->
+        <div class="col-12 bg-secondary" style="height: 300px;">
+            <h1 class="text-white text-center">banner</h1>
+        </div>
 
 
 
---
+++ utils/produtosCards.php
@@ -0,0 +1,30 @@
+<?php
+
+$produtosCard = [
+    "Monitor Gamer LG" => [
+        "assets/img/monitor_gamer.jpg",
+        "Monitor Gamer LG Ultrawide",
+        "599,99",
+        15
+    ],
+    "Console Nintendo Switch" => [
+        "assets/img/nintendo_switch.jpg",
+        "Console Nintendo Switch Diablo Bundle",
+        "1900,00",
+        8
+    ],
+    "Game Resident Evil 7" => [
+        "assets/img/resident_evill.jpg",
+        "Game Resident Evil 7 - PS4",
+        "96,99",
+        10
+    ],
+    "Mouse Pad Gamer" => [
+        "assets/img/mouse_pad02.jpg",
+        "Mouse Pad Gamer XYZ",
+        "22,90",
+        9
+    ]
+];
+
+?>
