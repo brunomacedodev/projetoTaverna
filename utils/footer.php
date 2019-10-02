d9043a616dde1a04a234721a428e988ec218bd98
brunomacedodev <macedodeoliveirabruno@gmail.com>
2019-10-01 22:39:12 -0300

    index_teste.php com include_once

---
+++ utils/footer.php
@@ -0,0 +1,24 @@
+<footer class="d-flex row justify-content-center mt-3">
+
+    <div class="row m-0">
+        <ul class="linksFooter p-0 m-0 d-flex flex-wrap">
+            <li class="efeitoLink mx-3">.sobre nós</li>
+            <li class="efeitoLink mx-3">.dúvidas</li>
+            <li class="efeitoLink mx-3">.central de atendimento</li>
+            <li class="efeitoLink mx-3">.venda com a gente</li>
+        </ul>
+    </div>
+
+    <div class="row p-3" style="width: 100%">
+        <div class="col-4 p-0 text-center">
+            <img src="assets\img\taverna_com_logo.svg" class="p-3" alt="" title="Taverna.com" style="width: 50%">
+        </div>
+        <div class="col-4"></div>
+        <div class="col-4 d-flex align-self-center justify-content-center">
+            <span class="btn btnPadrao border-0 p-0 mx-3" title="Youtube"><i class="fab fa-youtube"></i></span>
+            <span class="btn btnPadrao border-0 p-0 mx-3" title="Facebook"><i class="fab fa-facebook"></i></span>
+            <span class="btn btnPadrao border-0 p-0 mx-3" title="Instagram"><i class="fab fa-instagram"></i></span>
+            <span class="btn btnPadrao border-0 p-0 mx-3" title="Twitter"><i class="fab fa-twitter-square"></i></span>
+        </div>
+    </div>
+</footer>
---
+++ utils/header.php
@@ -0,0 +1,65 @@
+<header>
+            <div class="pos-f-t d-flex flex-wrap flex-row sticky-top">
+
+                <nav class="navbar navPrincipal col-12 py-2">
+                    <button class="btn btnPadrao navbar-toggler border-0 m-0 p-0" type="button" data-toggle="collapse"
+                        data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
+                        aria-expanded="false" aria-label="Alterna navegação">
+                        <i class="fas fa-bars "></i>
+                    </button>
+
+                    <div class="col-2 p-0 mx-3" title="Taverna.com">
+                        <img src="assets\img\taverna_com_logo.svg" alt="">
+                    </div>
+                    <div class="col-5 p-0">
+                        <div class="input-group">
+                            <input type="text" class="form-control bg-transparent text-white"
+                                placeholder="Busque por produtos, marcas e muito mais...">
+                            <div class="input-group-append">
+                                <button type="submit" class="btn btnPadrao border-0"><i
+                                        class="fas fa-search"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                    <ul class="iconsNav m-0 mx-0 p-0 col-2">
+                        <li class="btn btnPadrao border-0" title="Acesse sua conta" id="dropdownMenu"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i
+                                class="fas fa-user-circle"></i></li>
+                        <div class="dropdown-menu dropMenuBg" aria-labelledby="dropDownMenu">
+                            <ul class="pl-2">
+                                <li><a href="" class="efeitoLink">.entrar</a></li>
+                                <li><a href="" class="efeitoLink" title="Facebook">.entrar com <i
+                                            class="fab fa-facebook"></i></a></li>
+                                <li><a href="" class="efeitoLink" title="Google">.entrar com <i
+                                            class="fab fa-google"></i></a></li>
+                                <li><a href="" class="efeitoLink">.cadastrar</a></li>
+                                <div class="dropdown-divider"></div>
+                            </ul>
+                            <ul class="pl-2">
+                                <li><a href="" class="efeitoLink">.minha conta</a></li>
+                                <li><a href="" class="efeitoLink">.minhas compras</a></li>
+                            </ul>
+                        </div>
+                        <li class="btn btnPadrao border-0" title="Acesse seus favoritos"><i class="fas fa-heart"></i>
+                        </li>
+                        <li class="btn btnPadrao border-0" title="Acesse seu carrinho"><i
+                                class="fas fa-shopping-cart"></i>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="collapse" id="navbarToggleExternalContent">
+                    <div class="bg-dark p-0 justify-content-center">
+                        <div class="col navPrincipalTop">
+                            <ul class="menuCollapse ">
+                                <li><a href="" class="efeitoLink">.games</a></li>
+                                <li><a href="" class="efeitoLink">.colecionáveis</a></li>
+                                <li><a href="" class="efeitoLink">.moda</a></li>
+                                <li><a href="" class="efeitoLink">.informática</a></li>
+                                <li><a href="" class="efeitoLink">.livros e filmes</a></li>
+                                <li><a href="" class="efeitoLink font-weight-bold">.todos os departamentos</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
